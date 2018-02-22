ea970b709026a7a18f4c47a2afc7bf60a25894a2
Unknown <aznnoodle101@hotmail.com>
2018-02-21 17:46:49 -0800

    2/21/18
    Add stuff

---
+++ GAM-51_MyVector3/Assets/Scripts/Add.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Add : MonoBehaviour {
+    public GameObject A, B, Resultant;
+
+    private void Start() {
+        A = new GameObject("Vector A");
+        B = new GameObject("Vector B");
+        Resultant = new GameObject("Resultant");
+    }
+
+    private void Update() {
+        Debug.DrawLine(A.transform.forward, Vector3.zero, Color.red);
+        Debug.DrawLine(B.transform.forward, Vector3.zero, Color.green);
+        Resultant.transform.rotation = Vector3.(A.transform.forward + B.transform.forward);
+
+        Debug.DrawLine(A.transform.forward + B.transform.forward, Vector3.zero, Color.blue);
+    }
+}
