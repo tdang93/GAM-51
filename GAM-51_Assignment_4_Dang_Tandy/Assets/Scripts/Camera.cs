876c93458f3042c8911525003cf225632546596f
Unknown <aznnoodle101@hotmail.com>
2018-04-16 07:12:37 -0700

    some progress on osprey
    tilting, sensitivity on Mouse Y, torque

---
+++ GAM-51_Assignment_4_Dang_Tandy/Assets/Scripts/Camera.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+    public GameObject cameraX;
+    public GameObject cameraDock;
+    public GameObject Osprey;
+    public float moveX;
+    public float moveY;
+    public float zoom;
+
+    public void Start() {
+        cameraDock = new GameObject();
+        cameraDock.transform.position = cameraX.transform.position;
+        cameraDock.transform.parent = Osprey.transform;
+    }
+
+    public void Update() {
+        moveX = Input.GetAxis("Mouse X");
+        if(Mathf.Abs(moveX) < 0.25) { // dead zone for camera stability
+            moveX = 0;
+        }
+        moveY = Input.GetAxis("Mouse Y");
+        zoom = Input.GetAxis("Mouse ScrollWheel");
+    }
+
+    public void FixedUpdate() {
+        //cameraX.transform.Rotate(new Vector3(0, moveX, 0), Space.World);
+        //cameraY.transform.Rotate(new Vector3(-moveY, 0, 0), Space.World);
+        Vector3 look = Osprey.transform.position - cameraDock.transform.position;
+        gameObject.transform.Translate(look.normalized * zoom);
+    }
+}
--- GAM-51_Assignment_4_Dang_Tandy/Assets/Scripts/Osprey.cs
+++ GAM-51_Assignment_4_Dang_Tandy/Assets/Scripts/Osprey.cs
@@ -3,8 +3,8 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class Osprey : MonoBehaviour {
+    //public GameObject body;
     public new Rigidbody rigidbody;
-    public Vector3 CenterOfGravity;
     public float sensitivity = 1;
 
     [SerializeField] private FlyingMode flyingMode;
@@ -15,15 +15,21 @@ public class Osprey : MonoBehaviour {
     void Start() {
         if(gameObject.GetComponent<Rigidbody>() == null) {
             rigidbody = gameObject.AddComponent<Rigidbody>();
-            rigidbody.useGravity = false;
+            //rigidbody.useGravity = false;
         }
+        else {
+            rigidbody = gameObject.GetComponent<Rigidbody>();
+        }
+        //rigidbody.position = new Vector3(transform.position.x, -10, rotors.RotorL.transform.position.z);
+        rigidbody.centerOfMass = new Vector3(0, 0, 0);
 
         flyingMode = FlyingMode.Helicopter; // start in Helicopter FlyingMode
-        rotors.radius = 1;
+        //rotors.radius = 1;
+
     }
 
     void Update() {
-
+        //Debug.Log("Mouse X" + Input.GetAxis("Mouse X"));
     }
 
     void FixedUpdate() {
@@ -41,6 +47,7 @@ public class Osprey : MonoBehaviour {
     private void Move(FlyingMode FM) {
         if(FM == FlyingMode.Helicopter) {
             Vector3 pendingMove = Input_Positional;
+
             if(pendingMove.z != 0) {
                 float RotorLRotationX = rotors.RotorL.transform.rotation.eulerAngles.x;
                 float RotorRRotationX = rotors.RotorR.transform.rotation.eulerAngles.x;
@@ -54,7 +61,7 @@ public class Osprey : MonoBehaviour {
                     test -= 360;
                 }
                 if(pendingMove.z > 0) { // tilt Rotors forward for Z-axis movement
-                    if(test <= 90) {
+                    if(test <= 45) {
                         //Debug.Log("x: " + rotors.RotorL.transform.rotation.eulerAngles.x);
                         rotors.RotorL.transform.Rotate(rotateAmount); // set RotorL rotation, (+)
                         rotors.RotorR.transform.Rotate(rotateAmount); // set RotorR rotation, (+)
@@ -69,17 +76,17 @@ public class Osprey : MonoBehaviour {
                         //Debug.Log("z < 0: " + Input_Positional.z);
                     }
                 }
-                Debug.Log("test: " + test);
             }
+            //Debug.Log("test: " + test);
 
             Vector3 rotation = Vector3.zero;
             float newRotation = 0;
             if(pendingMove.y != 0) {
                 if(pendingMove.y > 0) {
-                    newRotation = 0.1f * Sensitivity;
+                    newRotation = 0.2f * Sensitivity;
                 }
                 else if(pendingMove.y < 0) {
-                    newRotation = -0.1f * Sensitivity;
+                    newRotation = -0.2f * Sensitivity;
                 }
                 rotors.spin += newRotation;
                 if(rotors.spin > 100) {
@@ -90,19 +97,55 @@ public class Osprey : MonoBehaviour {
                 }
             }
             rotation += new Vector3(0, rotors.spin, 0);
-            rotors.RotorL_Propeller.transform.Rotate(rotation);
-            rotors.RotorR_Propeller.transform.Rotate(-rotation);
+
+            Vector3 rbThrustLPosition = new Vector3(rotors.RotorL_Propeller.transform.position.x,
+                rotors.RotorL_Propeller.transform.position.y, rigidbody.transform.position.z);
+            Vector3 rbThrustRPosition = new Vector3(rotors.RotorR_Propeller.transform.position.x,
+                rotors.RotorR_Propeller.transform.position.y, rigidbody.transform.position.z);
+
+            Vector3 thrustL = rotors.RotorL.transform.up.normalized * rotors.thrust;
+            Vector3 thrustR = rotors.RotorR.transform.up.normalized * rotors.thrust;
+
+            float tiltAmount = 0; // 0 if no sideways input
+            if(pendingMove.x != 0) { // if attempting to turn/rotate left or right
+                tiltAmount = Mathf.Abs(pendingMove.x * 0.25f); // scale tilting by sideways input amount
+                if(pendingMove.x < 0) { // -x -> Osprey rotates left, rotorL spins CW faster than RotorR
+                    rotors.RotorL_Propeller.transform.Rotate(rotation * (1 + tiltAmount));
+                    thrustL *= 1 + tiltAmount;
+                    rigidbody.AddTorque(new Vector3(0, -(1 + tiltAmount), 0)); // CW
+
+                    rotors.RotorR_Propeller.transform.Rotate(-rotation * (1 - tiltAmount));
+                    thrustR *= 1 - tiltAmount;
+                }
+                else if(pendingMove.x > 0) { // +x -> Osprey rotates right, rotorR spins CCW faster than RotorL
+                    rotors.RotorL_Propeller.transform.Rotate(rotation * (1 - tiltAmount));
+                    thrustL *= 1 - tiltAmount;
+
+                    rotors.RotorR_Propeller.transform.Rotate(-rotation * (1 + tiltAmount));
+                    thrustR *= 1 + tiltAmount;
+                    rigidbody.AddTorque(new Vector3(0, (1 + tiltAmount), 0)); // CCW
+                }
+            }
+            else {
+                rotors.RotorL_Propeller.transform.Rotate(rotation);
+                rotors.RotorR_Propeller.transform.Rotate(-rotation);
+            }
             //Debug.Log("rotors.spin: " + rotors.spin);
             //Debug.Log("rotation.magnitude: " + rotation.magnitude);
 
-            rigidbody.AddForceAtPosition(100 * rotors.thrust, rotors.RotorL_Propeller.transform.position);
-            rigidbody.AddForceAtPosition(100 * rotors.thrust, rotors.RotorR_Propeller.transform.position);
+            rigidbody.AddForceAtPosition(thrustL, rbThrustLPosition);
+            rigidbody.AddForceAtPosition(thrustR, rbThrustRPosition);
+
+            Debug.DrawRay(rotors.RotorL.transform.position + new Vector3(0, 0, 0), thrustL, Color.yellow);
+            Debug.DrawRay(rotors.RotorR.transform.position + new Vector3(0, 0, 0), thrustR, Color.yellow);
+
+            //Debug.Log("force: " + rotors.thrust);
         }
     }
 
     private float Sensitivity {
         get {
-            float input = Input.GetAxis("Mouse ScrollWheel");
+            float input = Input.GetAxis("Mouse Y");
             //Debug.Log("scrollwheel: " + input);
             if(input > 0 && sensitivity <= 1.9f) { // upper limit is 2.0
                 sensitivity += 0.1f;
@@ -155,9 +198,10 @@ public class Osprey : MonoBehaviour {
         }
         public float spin; // speed: cycles per second
         public float efficiency; // thrust generated per cycle
-        public Vector3 thrust { // thrust = efficiency * area * speed
+        public float thrust { // thrust = efficiency * area * speed
             get {
-                return new Vector3(0, efficiency * Area *  spin, 0);
+                float magnitude = efficiency * Area *  spin;
+                return magnitude;
             }
         }
         // Area of circle = pi * r^2 ~ proportional  to: volume of air moved ~ thrust produced
